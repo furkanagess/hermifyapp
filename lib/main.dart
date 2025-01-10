8490f954c53e1f0a9a9a9151ca62c2781052e751
furkanagess <caglarrfurkann@gmail.com>
2025-01-11 00:55:24 +0300

    last_Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,38 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(const MyApp());
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:hermifyapp/base/constants/app_strings.dart';
+import 'package:hermifyapp/firebase_options.dart';
+import 'package:hermifyapp/provider/firebase_messaging_background_handler.dart';
+import 'package:hermifyapp/provider/home_view_model.dart';
+import 'package:hermifyapp/view/home_view.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stackTrace) {
+    FirebaseCrashlytics.instance.recordError(error, stackTrace, fatal: true);
+    return true;
+  };
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => HomeViewModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+final GlobalKey<ScaffoldMessengerState> scaffoldMassgengerKey =
+    GlobalKey<ScaffoldMessengerState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,15 +40,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: AppStrings.appName,
+      theme: ThemeData.dark(),
+      home: const HomeView(),
     );
   }
 }
